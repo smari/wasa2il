938124a7bd125976d9a4a5c93ecd6ce31d7c43e6
tomage <tomas@tomasarni.net>
2012-03-15 22:19:04 +0000

    Minor url cleanup and adding the django admin

--- wasa2il/urls.py
+++ wasa2il/urls.py
@@ -1,23 +1,26 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic import ListView, TemplateView, CreateView, UpdateView
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
 
 
 urlpatterns = patterns('',
-	(r'^$', 'core.views.home'),
+	# Uncomment the admin/doc line below to enable admin documentation:
+	(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	# Uncomment the next line to enable the admin:
+	(r'^admin/', include(admin.site.urls)),
 
-	(r'^polity/$',			login_required(ListView.as_view(model=Polity, context_object_name="polity"))),
-	(r'^polity/new/$',		login_required(CreateView.as_view(model=Polity, success_url="/polity/%(id)d/"))),
-	(r'^polity/(?P<pk>\d+)/edit/$',	login_required(UpdateView.as_view(model=Polity, success_url="/polity/%(id)d/"))),
-	(r'^polity/(?P<pk>\d+)/$',	login_required(DetailView.as_view(model=Polity, context_object_name="polity"))),
-
-	(r'^topic/$',			login_required(ListView.as_view(model=Topic, context_object_name="topic"))),
-	(r'^topic/new/$',		login_required(CreateView.as_view(model=Topic, success_url="/topic/%(id)d/"))),
-	(r'^topic/(?P<pk>\d+)/edit/$',	login_required(UpdateView.as_view(model=Topic, success_url="/topic/%(id)d/"))),
-	(r'^topic/(?P<pk>\d+)/$',	login_required(DetailView.as_view(model=Topic, context_object_name="topic"))),
-
-	(r'^issue/$',			login_required(ListView.as_view(model=Issue, context_object_name="issue"))),
-	(r'^issue/new/$',		login_required(CreateView.as_view(model=Issue, success_url="/issue/%(id)d/"))),
-	(r'^issue/(?P<pk>\d+)/edit/$',	login_required(UpdateView.as_view(model=Issue, success_url="/issue/%(id)d/"))),
-	(r'^issue/(?P<pk>\d+)/$',	login_required(DetailView.as_view(model=Issue, context_object_name="issue"))),
+	# Core app
+	(r'^', include('wasa2il.core.urls')),
 )
+
+urlpatterns += patterns('',
+	(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+		{'document_root': settings.STATIC_ROOT, 'show_indexes': True}
+	),
+)
+
