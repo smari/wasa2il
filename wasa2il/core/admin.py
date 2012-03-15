a1ec9bca8eb11600524ce3dc18200c2444bb0fec
Tómas Árni Jônasson <tomas@tomasarni.net>
2012-03-15 23:37:21 +0000

    Enhancing the admin a bit

--- wasa2il/core/admin.py
+++ wasa2il/core/admin.py
@@ -3,8 +3,31 @@ from django.contrib import admin
 
 from models import Polity, Topic, Issue
 
+def save_model(self, request, obj, form, change):
+	if getattr(obj, 'created_by', None) is None:
+		obj.created_by = request.user
+	obj.modified_by = request.user
+	obj.save()
+
+class BaseIssueAdmin(admin.ModelAdmin):
+	list_display = ['name', 'description']
+	search_fields = ['name', 'description']
+	prepopulated_fields = { 'slug': ['name'] }
+
+class PolityAdmin(BaseIssueAdmin):
+	list_display = BaseIssueAdmin.list_display + ['parent']
+
+class TopicAdmin(BaseIssueAdmin):
+	list_display = BaseIssueAdmin.list_display + ['polity']
+
+class IssueAdmin(BaseIssueAdmin):
+	list_display = BaseIssueAdmin.list_display + ['topics_str']
+
+class Delegate(admin.ModelAdmin):
+	list_display = ['user', 'delegate', 'base_issue']
+
 register = admin.site.register
 
-register(Polity)
-register(Topic)
-register(Issue)
+register(Polity, PolityAdmin)
+register(Topic, TopicAdmin)
+register(Issue, IssueAdmin)
