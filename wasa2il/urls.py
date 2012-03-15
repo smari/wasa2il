fa2a6a220274b7cefc189dce2f3a9184dbfec34c
Smári McCarthy <smari@immi.is>
2012-03-15 21:21:49 +0000

    Removed .pycs. Added generic class views.

--- wasa2il/urls.py
+++ wasa2il/urls.py
@@ -1,6 +1,20 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-	url(r'^$', 'core.views.home'),
-	
+	(r'^$', 'core.views.home'),
+
+	(r'^polity/$',			login_required(ListView.as_view(model=Polity, context_object_name="polity"))),
+	(r'^polity/new/$',		login_required(CreateView.as_view(model=Polity, success_url="/polity/%(id)d/"))),
+	(r'^polity/(?P<pk>\d+)/edit/$',	login_required(UpdateView.as_view(model=Polity, success_url="/polity/%(id)d/"))),
+	(r'^polity/(?P<pk>\d+)/$',	login_required(DetailView.as_view(model=Polity, context_object_name="polity"))),
+
+	(r'^topic/$',			login_required(ListView.as_view(model=Topic, context_object_name="topic"))),
+	(r'^topic/new/$',		login_required(CreateView.as_view(model=Topic, success_url="/topic/%(id)d/"))),
+	(r'^topic/(?P<pk>\d+)/edit/$',	login_required(UpdateView.as_view(model=Topic, success_url="/topic/%(id)d/"))),
+	(r'^topic/(?P<pk>\d+)/$',	login_required(DetailView.as_view(model=Topic, context_object_name="topic"))),
+
+	(r'^issue/$',			login_required(ListView.as_view(model=Issue, context_object_name="issue"))),
+	(r'^issue/new/$',		login_required(CreateView.as_view(model=Issue, success_url="/issue/%(id)d/"))),
+	(r'^issue/(?P<pk>\d+)/edit/$',	login_required(UpdateView.as_view(model=Issue, success_url="/issue/%(id)d/"))),
+	(r'^issue/(?P<pk>\d+)/$',	login_required(DetailView.as_view(model=Issue, context_object_name="issue"))),
 )
