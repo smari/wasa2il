24e2931ab3a50119b3ce5a57f0a6e76b307b36c0
tomage <tomas@tomasarni.net>
2012-03-15 22:38:27 +0000

    Fixing and simplifying models. Making BaseIssue a non-abstract class. Adding basic admin stuff. Adding an ENV file (useful for setting PYTHONPATH etc.)

--- wasa2il/core/models.py
+++ wasa2il/core/models.py
@@ -1,44 +1,35 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 from fields import NameField, NameSlugField, CreatedField, ModifiedField, AutoUserField
+from base_classes import getCreationBase
 
 nullblank = { 'null': True, 'blank': True }
 
-def getCreationBase(prefix=None):
-	class CreationBase(models.Model):
-		if prefix:
-			created_by		= AutoUserField(related_name='%s_created_by'%prefix)
-			modified_by		= AutoUserField(related_name='%s_modified_by'%prefix)
-		else:
-			created_by		= AutoUserField(related_name='created_by')
-			modified_by		= AutoUserField(related_name='modified_by')
-		created			= CreatedField()
-		modified		= ModifiedField()
-		class Meta:
-			abstract = True
-	return CreationBase
 
-def getBaseIssue(prefix=None):
-	class BaseIssue(getCreationBase(prefix)):
-		name			= NameField()
-		slug			= NameSlugField()
-		description		= models.TextField(**nullblank)
-		class Meta:
-			abstract = True
-	return BaseIssue
 
-class Polity(getBaseIssue('polity')):
-	parent			= models.ForeignKey('Polity')
+class BaseIssue(models.Model):
+	name			= NameField()
+	slug			= NameSlugField()
+	description		= models.TextField(**nullblank)
+
+class Polity(BaseIssue, getCreationBase('polity')):
+	parent			= models.ForeignKey('Polity', **nullblank)
 	invite_threshold	= models.IntegerField(default=3)
 
 
-class Topic(getBaseIssue('topic')):
+class Topic(BaseIssue, getCreationBase('topic')):
 	polity			= models.ForeignKey(Polity)
 
-class Issue(getBaseIssue('issue')):
+class Issue(BaseIssue, getCreationBase('issue')):
 	topic			= models.ManyToManyField(Topic)
 
 class Comment(getCreationBase('comment')):
 	comment			= models.TextField()
 	issue			= models.ForeignKey(Issue)
 
+class Delegate(models.Model):
+	user			= models.ForeignKey(User)
+	delegate		= models.ForeignKey(User, related_name='delegate_user')
+	base_issue		= models.ForeignKey(BaseIssue)
