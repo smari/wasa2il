43892aaf79b340634d95e0cdae6ee5f12fd3a4c7
tomage <tomas@tomasarni.net>
2012-03-15 22:00:34 +0000

    Update models. Adding Comment and using the generic fields.

--- wasa2il/core/models.py
+++ wasa2il/core/models.py
@@ -1,23 +1,37 @@
 from django.db import models
 
+from fields import NameField, NameSlugField, CreatedField, ModifiedField, AutoUserField
 
-class BaseIssue(models.Model):
-	name			= models.CharField(max_length=200)
-	description		= models.TextField()
+nullblank = { 'null': True, 'blank': True }
 
+
+class CreationBase(models.Model):
+	created_by		= AutoUserField()
+	modified_by		= AutoUserField()
+	created			= CreatedField()
+	modified		= ModifiedField()
 	class Meta:
 		abstract = True
 
+class BaseIssue(CreationBase):
+	name			= NameField()
+	slug			= NameSlugField()
+	description		= models.TextField(**nullblank)
+	class Meta:
+		abstract = True
 
 class Polity(BaseIssue):
-	parent			= models.ForeignKey(Polity)
+	parent			= models.ForeignKey('Polity')
 	invite_threshold	= models.IntegerField(default=3)
 
 
 class Topic(BaseIssue):
 	polity			= models.ForeignKey(Polity)
 
-
 class Issue(BaseIssue):
 	topic			= models.ManyToManyField(Topic)
-	
+
+class Comment(CreationBase):
+	comment			= models.TextField()
+	issue			= models.ForeignKey(Issue)
+
