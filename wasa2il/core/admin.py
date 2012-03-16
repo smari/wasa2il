6d4e90c3f6fc74c8530e0e4a9c69dee2b452d306
Tómas Árni Jônasson <tomas@tomasarni.net>
2012-03-16 13:40:29 +0000

    Playing around with Admin. Testing the getDerivedAdmin idea.

--- wasa2il/core/admin.py
+++ wasa2il/core/admin.py
@@ -1,7 +1,17 @@
 
 from django.contrib import admin
 
-from models import Polity, Topic, Issue
+from models import Polity, Topic, Issue, VoteOption
+
+
+def getDerivedAdmin(base_admin, **kwargs):
+	class DerivedAdmin(base_admin):
+		pass
+	derived = DerivedAdmin
+	for k,v in kwargs.iteritems():
+		setattr(derived, k, getattr(base_admin, k, []) + v)
+	return derived
+
 
 def save_model(self, request, obj, form, change):
 	if getattr(obj, 'created_by', None) is None:
@@ -9,25 +19,52 @@ def save_model(self, request, obj, form, change):
 	obj.modified_by = request.user
 	obj.save()
 
-class BaseIssueAdmin(admin.ModelAdmin):
-	list_display = ['name', 'description']
-	search_fields = ['name', 'description']
+
+class NameSlugAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, { 'fields': ['name', 'slug'] }),
+	]
 	prepopulated_fields = { 'slug': ['name'] }
+	list_display = ['name', 'get_url']
+	search_fields = ['name']
+
+
+BaseIssueAdmin = getDerivedAdmin(NameSlugAdmin,
+		list_display=['description'],
+		search_fields=['description'],
+	)
+BaseIssueAdmin.fieldsets = [
+		NameSlugAdmin.fieldsets[0],
+		(None, { 'fields': ['description'] }),
+	]
+
 
 class PolityAdmin(BaseIssueAdmin):
+	fieldsets = None
 	list_display = BaseIssueAdmin.list_display + ['parent']
 
+
 class TopicAdmin(BaseIssueAdmin):
+	fieldsets = None
 	list_display = BaseIssueAdmin.list_display + ['polity']
 
+
 class IssueAdmin(BaseIssueAdmin):
+	fieldsets = None
 	list_display = BaseIssueAdmin.list_display + ['topics_str']
+
 
 class Delegate(admin.ModelAdmin):
 	list_display = ['user', 'delegate', 'base_issue']
 
-register = admin.site.register
 
+class VoteOptionAdmin(NameSlugAdmin):
+	pass
+
+
+# Register the admins
+register = admin.site.register
 register(Polity, PolityAdmin)
 register(Topic, TopicAdmin)
 register(Issue, IssueAdmin)
+register(VoteOption, VoteOptionAdmin)
