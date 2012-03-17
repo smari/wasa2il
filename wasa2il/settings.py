29e4dee2e4a0607626dc259f5cd38a25065cf33d
Tómas Árni Jônasson <tomas@tomasarni.net>
2012-03-17 20:49:13 +0000

    Fixing the STATIC_URL... apparently we don't need the http://... part

--- wasa2il/settings.py
+++ wasa2il/settings.py
@@ -62,7 +62,7 @@ STATIC_ROOT = here('static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://wasa2il.local/static/'
+STATIC_URL = 'static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
