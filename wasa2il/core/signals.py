9be3eaa9243932e63d04e46e501a770821960c2b
Tómas Árni Jónasson <tomas@clara.is>
2012-12-09 22:28:31 +0000

    Creating UserProfile for new Users using post_save signal

---
+++ wasa2il/core/signals.py
@@ -0,0 +1,11 @@
+
+from django.db.models import signals
+from django.dispatch import receiver
+from models import UserProfile
+from django.contrib.admin.models import User
+
+
+@receiver(signals.post_save, sender=User)
+def create_userprofile(sender, instance, created, **kwargs):
+	if created:
+		UserProfile.objects.create(user=instance)
