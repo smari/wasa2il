575fa087923dd0aa0ca3a5c7b56b9ee059ee4bd9
Tómas Árni Jónasson <tomas@clara.is>
2012-12-12 19:32:26 +0000

    Minor fixes and some PEP8 adjustments for model files

--- wasa2il/core/base_classes.py
+++ wasa2il/core/base_classes.py
@@ -3,11 +3,14 @@ from django.db import models
 from fields import AutoUserField, CreatedField, ModifiedField, NameField, NameSlugField
 from django.utils.safestring import mark_safe
 
+
 class NameSlugBase(models.Model):
 	name			= NameField()
 	slug			= NameSlugField()
+
 	class Meta:
 		abstract = True
+
 	def get_url(self, anchor=True):
 		if anchor:
 			print type(mark_safe(u'<a href="%s">%s</a>' % (self.slug, self.name)))
@@ -15,15 +18,20 @@ class NameSlugBase(models.Model):
 		return u'/slug/%s/' % self.slug
 	get_url.allow_tags = True
 	get_url.short_description = 'Slug-URL'
+
 	def __unicode__(self):
 		return u'%s' % (self.name)
 
+
 def getCreationBase(prefix):
+
 	class CreationBase(models.Model):
 		created_by		= AutoUserField(related_name='%s_created_by'%prefix)
 		modified_by		= AutoUserField(related_name='%s_modified_by'%prefix)
 		created			= CreatedField()
 		modified		= ModifiedField()
+
 		class Meta:
 			abstract = True
+
 	return CreationBase
