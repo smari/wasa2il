3d1ad8cd92264a4b8cec8f03930b2d4309c8e563
Smári McCarthy <smari@immi.is>
2012-12-30 10:12:59 +0000

    Comments.

--- wasa2il/core/models.py
+++ wasa2il/core/models.py
@@ -22,7 +22,6 @@ class UserProfile(models.Model):
 	"""A user's profile data. Contains various informative areas, plus various settings."""
 	user			= models.OneToOneField(User)
 
-
 	# User information
 	displayname		= models.CharField(max_length="255", verbose_name="Display name", help_text="The name to display on the site.", **nullblank)
 	email_visible		= models.BooleanField(default=False, help_text="Whether to display your email address on your profile page.")
