d7ed7c6d097cdc5f6f7e43b7286109038d7a6628
root <root@immi.is>
2013-01-24 12:27:16 +0000

    .

--- wasa2il/settings.py
+++ wasa2il/settings.py
@@ -19,10 +19,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': here('wasa2il.sqlite3'),                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'wasa2il',                      # Or path to database file if using sqlite3.
+        'USER': 'wasa2il',                      # Not used with sqlite3.
+        'PASSWORD': '2.21gigawatts',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
