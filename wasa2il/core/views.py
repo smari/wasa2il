4d8492f6699b17ebeedd44ed3bc00c9f9b27fa41
Smári McCarthy <smari@immi.is>
2013-02-07 03:11:58 +0000

    Show elections in polity overview.

--- wasa2il/core/views.py
+++ wasa2il/core/views.py
@@ -215,6 +215,7 @@ class PolityDetailView(DetailView):
 		ctx["politytopics"] = self.object.get_topic_list(self.request.user)
 		ctx["delegation"] = self.object.get_delegation(self.request.user)
 		ctx["newissues"] = self.object.issue_set.filter(deadline_votes__gt=datetime.now())[:10]
+		ctx["newelections"] = self.object.election_set.filter(deadline_votes__gt=datetime.now())[:10]
 		ctx["settings"] = settings
 		# ctx["delegations"] = Delegate.objects.filter(user=self.request.user, polity=self.object)
 
