d3cf25abdb3e9205fc704ebcc6f44008bc1de82a
root <root@immi.is>
2013-02-21 19:17:10 +0000

    Local changes.

--- wasa2il/core/templatetags/wasa2il.py
+++ wasa2il/core/templatetags/wasa2il.py
@@ -41,28 +41,29 @@ import Image
 
 @register.filter(name="thumbnail")
 def thumbnail(file, size='104x104'):
-    # defining the size
-    x, y = [int(x) for x in size.split('x')]
-    # defining the filename and the miniature filename
-    print "GENERATING THUMBNAIL file:%s, size:%s" % (file, size)
-    print type(file)
-    filehead, filetail = os.path.split(file.path)
-    basename, format = os.path.splitext(filetail)
-    miniature = basename + '_' + size + format
-    filename = file.path
-    miniature_filename = os.path.join(filehead, miniature)
-    filehead, filetail = os.path.split(file.url)
-    miniature_url = filehead + '/' + miniature
-    if os.path.exists(miniature_filename) and os.path.getmtime(filename)>os.path.getmtime(miniature_filename):
-        os.unlink(miniature_filename)
-    # if the image wasn't already resized, resize it
-    if not os.path.exists(miniature_filename):
-        image = Image.open(filename)
-        image.thumbnail([x, y], Image.ANTIALIAS)
-        try:
-            image.save(miniature_filename, image.format, quality=90, optimize=1)
-        except:
-            image.save(miniature_filename, image.format, quality=90)
+	try:
+		# defining the size
+		x, y = [int(x) for x in size.split('x')]
+		# defining the filename and the miniature filename
+		filehead, filetail = os.path.split(file.path)
+		basename, format = os.path.splitext(filetail)
+		miniature = basename + '_' + size + format
+		filename = file.path
+		miniature_filename = os.path.join(filehead, miniature)
+		filehead, filetail = os.path.split(file.url)
+		miniature_url = filehead + '/' + miniature
+		if os.path.exists(miniature_filename) and os.path.getmtime(filename)>os.path.getmtime(miniature_filename):
+			os.unlink(miniature_filename)
+		# if the image wasn't already resized, resize it
+		if not os.path.exists(miniature_filename):
+			image = Image.open(filename)
+			image.thumbnail([x, y], Image.ANTIALIAS)
+			try:
+				image.save(miniature_filename, image.format, quality=90, optimize=1)
+			except:
+				image.save(miniature_filename, image.format, quality=90)
 
-    return miniature_url
+		return miniature_url
+	except:
+		return ""
 
