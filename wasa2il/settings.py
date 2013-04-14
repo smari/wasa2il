f8ad979e98f61854ea52e337beb97e17654b3daf
Tómas Árni Jónasson <tomas@clara.is>
2013-04-14 18:31:12 +0000

    Fixing static urls a bit in settings

--- wasa2il/settings.py
+++ wasa2il/settings.py
@@ -71,7 +71,7 @@ MEDIA_URL = '/uploads/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = here('core/static/')
+STATIC_ROOT = here('static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -87,7 +87,6 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	here('static/'),
 )
 
 # List of finder classes that know how to find static files in
