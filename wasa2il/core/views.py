7d7a4e689176d05eb1b3e0ce98e0f04d77af3dc0
Tómas Árni Jónasson <tomas@clara.is>
2013-04-14 21:30:19 +0000

    Loads of changes - comment detail not available

--- wasa2il/core/views.py
+++ wasa2il/core/views.py
@@ -239,7 +239,7 @@ class PolityDetailView(DetailView):
 		ctx["membership_requests"] = MembershipRequest.objects.filter(polity=self.object, fulfilled=False)
 		ctx["politytopics"] = self.object.get_topic_list(self.request.user)
 		ctx["delegation"] = self.object.get_delegation(self.request.user)
-		ctx["newissues"] = self.object.issue_set.filter(deadline_votes__gt=datetime.now())[:10]
+		ctx["newissues"] = self.object.issue_set.order_by("deadline_votes").filter(deadline_votes__gt=datetime.now())[:15]
 		ctx["newelections"] = self.object.election_set.filter(deadline_votes__gt=datetime.now())[:10]
 		ctx["settings"] = settings
 		# ctx["delegations"] = Delegate.objects.filter(user=self.request.user, polity=self.object)
