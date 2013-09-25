a82be1b3b176b628c102152c415c54cd0f3969a9
Tómas Árni Jónasson <tomas@clara.is>
2013-09-25 20:09:00 +0000

    Err.. fixing a bug introduced by overworked PEP8 refactoring...

--- wasa2il/core/signals.py
+++ wasa2il/core/signals.py
@@ -2,7 +2,7 @@
 from django.db.models import signals
 from django.dispatch import receiver
 from models import UserProfile
-from django.contrib.admin.models import User
+from django.contrib.auth.models import User
 
 
 @receiver(signals.post_save, sender=User)
