9fa203bc5e5c2708540da50fc8877fce7add4767
Helgi Hrafn Gunnarsson <helgi@binary.is>
2014-05-20 17:17:21 +0000

    Crash-fix for when a user logs in who is not in member database. Entire process needs reviewing.

--- wasa2il/gateway/icepirate.py
+++ wasa2il/gateway/icepirate.py
@@ -11,23 +11,19 @@ def configure_polities_by_remote_groups(user):
     key = settings.ICEPIRATE['key']
     kennitala = user.userprofile.kennitala
 
-    try:
-        request = urllib.urlopen('%s/member/api/get/kennitala/%s?json_api_key=%s' % (url, kennitala, key))
-        content = request.read()
-    except IOError:
-        raise IOError('IcePirate could not be reached. Make sure IcePirate is running according to ICEPIRATE setting in local_settings.py')
-
+    request = urllib.urlopen('%s/member/api/get/kennitala/%s?json_api_key=%s' % (url, kennitala, key))
+    content = request.read()
     try:
         remote_object = json.loads(content)
-    except ValueError:
-        raise ValueError('IcePirate returned wrong object (user probably doesn\'t exist in IcePirate)')
 
-    icepirate_groups = remote_object['data']['groups']
+        icepirate_groups = remote_object['data']['groups']
 
-    for polity in Polity.objects.filter(slug__in = icepirate_groups):
-        polity.members.add(user)
+        for polity in Polity.objects.filter(slug__in = icepirate_groups):
+            polity.members.add(user)
 
-    for polity in Polity.objects.exclude(slug__isnull=True).exclude(slug__exact=''):
-    	if polity.is_member(user) and polity.slug not in icepirate_groups:
-    		polity.members.remove(user)
+        for polity in Polity.objects.exclude(slug__isnull=True).exclude(slug__exact=''):
+            if polity.is_member(user) and polity.slug not in icepirate_groups:
+                polity.members.remove(user)
+    except:
+        pass
 
