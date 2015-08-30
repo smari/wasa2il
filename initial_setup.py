8f4be7fb1266915be6e0d77a4d7e9ba51da4063f
Björgvin Ragnarsson <nifgraup@gmail.com>
2015-08-31 01:05:08 +0300

    deduplicate get_random_string

--- initial_setup.py
+++ initial_setup.py
@@ -5,23 +5,7 @@ import os
 import subprocess
 import fileinput
 import shutil
-
-import random
-random = random.SystemRandom()
-
-
-def get_random_string(length=12,
-                      allowed_chars='abcdefghijklmnopqrstuvwxyz'
-                                    'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'):
-    """
-    Returns a securely generated random string.
-
-    The default length of 12 with the a-z, A-Z, 0-9 character set returns
-    a 71-bit value. log_2((26+26+10)^12) =~ 71 bits.
-
-    Taken from the django.utils.crypto module.
-    """
-    return ''.join(random.choice(allowed_chars) for i in range(length))
+from django.utils.crypto import get_random_string
 
 
 def get_secret_key():
