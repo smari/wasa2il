04f4c32f64553d29ff517a1faa3ce7bd23d12427
Helgi Hrafn Gunnarsson <helgi@binary.is>
2017-08-29 21:46:40 +0000

    Removal of django-babel in favor of native Django translation mechanism.

--- core/management/commands/compilemessages.py
+++
@@ -1,7 +0,0 @@
-from django.core.management import BaseCommand
-from babel.messages.frontend import CommandLineInterface
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        cli = CommandLineInterface()
-        cli.run("pybabel compile -d wasa2il/locale -D django".split(" "))
