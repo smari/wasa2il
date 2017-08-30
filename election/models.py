35b89395c738e7e6c51a5b6f0339b75fcaa59390
Helgi Hrafn Gunnarsson <helgi@binary.is>
2017-08-30 18:58:10 +0000

    Missing import resolved.

--- election/models.py
+++ election/models.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime
 
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 from django.db import transaction
 from django.utils.translation import ugettext_lazy as _
