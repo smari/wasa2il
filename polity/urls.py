69e77d29eb87cacdde5d81bf11eef73a53992754
Helgi Hrafn Gunnarsson <helgi@binary.is>
2017-09-25 16:25:30 +0000

    Polity main page never cached.

--- polity/urls.py
+++ polity/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.cache import cache_page
-from django.views.decorators.vary import vary_on_headers
+from django.views.decorators.cache import never_cache
 from django.views.generic import UpdateView
 
 from polity.models import Polity
@@ -15,7 +14,5 @@ urlpatterns = [
     url(r'^polity/new/$', login_required(PolityCreateView.as_view())),
     url(r'^polity/(?P<pk>\d+)/edit/$', login_required(UpdateView.as_view(model=Polity, success_url="/polity/%(id)d/"))),
     url(r'^polity/(?P<pk>\d+)/(?P<action>\w+)/$', login_required(PolityDetailView.as_view())),
-    url(r'^polity/(?P<pk>\d+)/$',
-        cache_page(60*5)(vary_on_headers('Cookie')(
-            PolityDetailView.as_view())), name='polity_detail'),
+    url(r'^polity/(?P<pk>\d+)/$', never_cache(PolityDetailView.as_view()), name='polity'),
 ]
