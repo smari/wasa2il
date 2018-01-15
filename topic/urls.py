7b9e300424f0a5e40c1a1cc4b4255e85203ff378
Smári McCarthy <smari@smarimccarthy.is>
2018-01-15 20:15:55 +0000

    Add topics/

--- topic/urls.py
+++ topic/urls.py
@@ -5,12 +5,15 @@ from topic.dataviews import topic_showstarred
 from topic.dataviews import topic_star
 from topic.views import topic_add_edit
 from topic.views import topic_view
+from topic.views import topic_list
 
 
 urlpatterns = [
     url(r'^polity/(?P<polity_id>\d+)/topic/new/$', topic_add_edit, name='topic_add'),
     url(r'^polity/(?P<polity_id>\d+)/topic/(?P<topic_id>\d+)/edit/$', topic_add_edit, name='topic_edit'),
     url(r'^polity/(?P<polity_id>\d+)/topic/(?P<topic_id>\d+)/$', topic_view, name='topic'),
+    url(r'^polity/(?P<polity_id>\d+)/topics/$', topic_list, name='topics'),
+
 
     url(r'^api/topic/star/$', topic_star),
     url(r'^api/topic/showstarred/$', topic_showstarred),
