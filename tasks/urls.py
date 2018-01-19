279f87871ddf90895596954a3c1dbe95cbf8d008
Smári McCarthy <smari@smarimccarthy.is>
2018-01-19 22:37:40 +0000

    task_add_edit and template for it

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,11 +3,11 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
 
 from tasks.models import Task
-from tasks.views import task_list
+from tasks.views import task_list, task_add_edit
 
 urlpatterns = [
     url(r'^polity/(?P<polity_id>\d+)/tasks/$', task_list, name='tasks'),
-#    url(r'^polity/(?P<polity_id>\d+)/tasks/(?P<task_id>\d+)/edit/$', issue_add_edit, name='issue_edit'),
-#    url(r'^polity/(?P<polity_id>\d+)/issue/new/(documentcontent/(?P<documentcontent_id>\d+)/)?$', issue_add_edit, name='issue_add'),
-#    url(r'^polity/(?P<polity_id>\d+)/issue/(?P<issue_id>\d+)/$', never_cache(issue_view), name='issue'),
+    url(r'^polity/(?P<polity_id>\d+)/tasks/(?P<task_id>\d+)/edit/$', task_add_edit, name='task_edit'),
+    url(r'^polity/(?P<polity_id>\d+)/tasks/new/$', task_add_edit, name='task_add'),
+    # url(r'^polity/(?P<polity_id>\d+)/tasks/(?P<issue_id>\d+)/$', never_cache(issue_view), name='issue'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -7,12 +7,11 @@ from django.shortcuts import render
 
 from tasks.models import Task
 from polity.models import Polity
+from tasks.forms import TaskForm
 
 def task_list(request, polity_id):
     polity = get_object_or_404(Polity, id=polity_id)
     tasks = polity.task_set.order_by('-created')
-
-
 
     ctx = {
         'polity': polity,
@@ -22,3 +21,35 @@ def task_list(request, polity_id):
         'user_is_wrangler': polity.is_wrangler(request.user),
     }
     return render(request, 'tasks/task_list.html', ctx)
+
+
+@login_required
+def task_add_edit(request, polity_id, task_id=None):
+    polity = get_object_or_404(Polity, id=polity_id)
+    if not (polity.is_member(request.user) or polity.is_wrangler(request.user)):
+        raise PermissionDenied()
+
+    if task_id:
+        task = get_object_or_404(Task, id=task_id, polity=polity)
+        # We don't want to edit anything that has already done.
+        if task.is_done:
+            raise PermissionDenied()
+    else:
+        task = Task(polity=polity)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save()
+            return redirect(reverse('task_detail', args=(polity_id, task.id)))
+    else:
+        form = TaskForm(instance=task)
+
+    ctx = {
+        'polity': polity,
+        'form': form,
+        'user_is_member': polity.is_member(request.user),
+        'user_is_officer': polity.is_officer(request.user),
+        'user_is_wrangler': polity.is_wrangler(request.user),
+    }
+    return render(request, 'tasks/task_add_edit.html', ctx)
