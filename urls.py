f28cb681f17343456e45a391b912d7f3bd255f31
Helgi Hrafn Gunnarsson <helgi@binary.is>
2018-01-29 21:24:14 +0000

    Ancient temporary provision removed, aimed at providing compatibility until packages were updated.

--- urls.py
+++ urls.py
@@ -34,18 +34,6 @@ urlpatterns = [
     url(r'^accounts/verify/', core_views.verify),
     url(r'^accounts/sso/', core_views.sso),
 
-    # - START OF TEMPORARY COMPATIBILITY HACK -
-    # IMPORTANT! The entire ^accounts/password/ section is here as a temporary hack until the official django-registration gets fixed!
-    # See discussion: http://stackoverflow.com/questions/19985103/django-1-6-and-django-registration-built-in-authentication-views-not-picked-up
-    # This may break in the future during an update of the django-registration package. Try removing this section.
-    url(r'^accounts/password/change/$', auth_views.password_change, name='password_change'),
-    url(r'^accounts/password/change/done/$', auth_views.password_change_done, name='password_change_done'),
-    url(r'^accounts/password/reset/$', auth_views.password_reset, name='password_reset'),
-    url(r'^accounts/password/reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
-    url(r'^accounts/password/reset/complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
-    url(r'^accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
-    # - END OF TEMPORARY COMPATIBILITY HACK -
-
     url(r'^accounts/', include('registration.urls')),
 
     url(r'^help/$', TemplateView.as_view(template_name='help/is/index.html')),
