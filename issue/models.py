smari/wasa2il

commit ad0cfd75d3c6f68b70acfcf5aec3d359540d1c51

Function for checking if a majority was reached always reports false if issue hasn't concluded.

issue/models.py
@@ -193,6 +193,9 @@ class Issue(models.Model):
             return []
 
     def majority_reached(self):
+        if not self.issue_state() == 'concluded' or not self.is_processed:
+            return False
+
         result = False
 
         if self.special_process == 'accepted_at_assembly':

commit 5309dabaeecbc4554fc07655e47179fe3156531d

Fixed bug introduced in commit ad0cfd7, preventing issues from being accepted during processing.

issue/models.py
@@ -193,7 +193,7 @@ class Issue(models.Model):
             return []
 
     def majority_reached(self):
-        if not self.issue_state() == 'concluded' or not self.is_processed:
+        if not self.issue_state() == 'concluded':
             return False
 
         result = False