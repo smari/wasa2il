b1197bec778d99dbe8e1bde2935292ec24b10fc5
Helgi Hrafn Gunnarsson <helgi@binary.is>
2020-02-21 20:09:47 +0000

    Errors logged to file now, so that those investigating errors don't need to be on a mailing list and rely on email actually working.

--- wasa2il/settings.py
+++ wasa2il/settings.py
@@ -331,34 +331,27 @@ AUTHENTICATION_BACKENDS = (
     'core.authentication.EmailAuthenticationBackend',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
+
+# See http://docs.djangoproject.com/en/dev/topics/logging
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': 'errors.log',
+        },
     },
-    'filters': {
-         'require_debug_false': {
-             '()': 'django.utils.log.RequireDebugFalse'
-         }
-     },
     'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
             'propagate': True,
         },
-    }
+    },
 }
+
 
 TERMS_EXCLUDE_URL_PREFIX_LIST = (
     '/admin/',
